f73d2f7c04c7912283983d05532b0b7f21a91744
Greivin <greivin95@gmail.com>
2020-07-26 14:15:18 -0600

    script tablas

--- scriptTablasInventario.sql
+++ scriptTablasInventario.sql
@@ -7,3 +7,28 @@ create table inventario.articuloPrecio
 CONSTRAINT codigoArticulo_FK FOREIGN KEY (codigoArticulo)
   REFERENCES inventario.articulos (codigoArticulo) ENABLE);
   
+create table inventario.articuloExistencia
+(codigoArticulo NUMBER(9,0), existencia NUMBER(6,0),
+CONSTRAINT codigoArticulo_FK FOREIGN KEY (codigoArticulo)
+  REFERENCES inventario.articulos (codigoArticulo) ENABLE);
+
+create table inventario.Compras
+(codigoCompra NUMBER(9,0), factura NUMBER(9,0), codigoProveedor number (6,0), montoCompra NUMBER(9,0), fechaCompra Date, usuarioRegistro varchar2(20 BYTE),
+CONSTRAINT codigoCompra_PK PRIMARY KEY (codigoCompra),
+CONSTRAINT codigoProveedor_FK FOREIGN KEY (codigoProveedor)
+  REFERENCES inventario.Proveedor (codigoProveedor) ENABLE,
+CONSTRAINT usuarioRegistro_FK FOREIGN KEY (usuarioRegistro)
+  REFERENCES inventario.Usuario (usuario) ENABLE);
+
+  
+create table inventario.ComprasDetalle 
+(codigoCompra NUMBER(9,0),codigoArticulo NUMBER(9,0), cantidadCompra NUMBER(6,0), montoCosto NUMBER(9,0),
+CONSTRAINT codigoCompra_FK FOREIGN KEY (codigoCompra)
+  REFERENCES inventario.Compras (codigoCompra) ENABLE,
+CONSTRAINT codigoArticulo_FK FOREIGN KEY (codigoArticulo)
+  REFERENCES inventario.articulos (codigoArticulo) ENABLE);
+  
+create table inventario.Ventas  
+(codigoVenta NUMBER(9,0), factura NUMBER(9,0), identificacionCliente NUMBER(9,0), montoVenta NUMBER(9,0),fechaVenta Date, usuarioRegistro varchar2(20 BYTE),
+CONSTRAINT codigoVenta_PK PRIMARY KEY (codigoVenta));
+  
