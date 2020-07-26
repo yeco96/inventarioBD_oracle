9521699ee52bd67ff460d42946d09b8d866983d1
Greivin <greivin95@gmail.com>
2020-07-26 15:35:32 -0600

    se agregaron tablas

--- scriptTablasInventario.sql
+++ scriptTablasInventario.sql
@@ -2,6 +2,12 @@ create table inventario.articulos
 (codigoArticulo NUMBER(9,0), descripcion VARCHAR2(40 BYTE), cantMinima NUMBER(6,0), fechaCreacion DATE,
 CONSTRAINT codigoArticulo_PK PRIMARY KEY (codigoArticulo));
 
+create table inventario.Proveedor
+(codigoProveedor NUMBER(6,0), nombre VARCHAR2(20 BYTE), fechaIngreso DATE);
+
+create table inventario.Usuario
+(usuario VARCHAR2(20 BYTE), nombre VARCHAR2(20 BYTE), contrasena password);
+
 create table inventario.articuloPrecio
 (codigoArticulo NUMBER(9,0), precio NUMBER(6,0), tarifaImpuesto NUMBER(6,0), porcentajeUtilidad NUMBER(6,0), montoCosto NUMBER(9,0),
 CONSTRAINT codigoArticulo_FK FOREIGN KEY (codigoArticulo)
@@ -30,5 +36,21 @@ CONSTRAINT codigoArticulo_FK FOREIGN KEY (codigoArticulo)
   
 create table inventario.Ventas  
 (codigoVenta NUMBER(9,0), factura NUMBER(9,0), identificacionCliente NUMBER(9,0), montoVenta NUMBER(9,0),fechaVenta Date, usuarioRegistro varchar2(20 BYTE),
-CONSTRAINT codigoVenta_PK PRIMARY KEY (codigoVenta));
+CONSTRAINT codigoVenta_PK PRIMARY KEY (codigoVenta),
+CONSTRAINT usuarioRegistro_FK FOREIGN KEY (usuarioRegistro)
+  REFERENCES inventario.Usuario (usuario) ENABLE);
+  
+create table inventario.VentasDetalle 
+(codigoVenta NUMBER(9,0),codigoArticulo NUMBER(9,0), precio NUMBER(6,0),
+CONSTRAINT codigoVenta_FK FOREIGN KEY (codigoVenta)
+  REFERENCES inventario.Ventas (codigoVenta) ENABLE,
+CONSTRAINT codigoArticulo_FK FOREIGN KEY (codigoArticulo)
+  REFERENCES inventario.articulos (codigoArticulo) ENABLE);
+  
+create table inventario.Movimientos
+(codigoArticulo NUMBER(9,0), concepto varchar2 (20 BYTE), cantidadAntes number(6,0), cantidad number(6,0), cantidadDespues number(6,0), fechaRegistro Date, usuarioRegistro varchar2(20 BYTE),
+CONSTRAINT codigoArticulo_FK FOREIGN KEY (codigoArticulo)
+  REFERENCES inventario.articulos (codigoArticulo) ENABLE,
+ CONSTRAINT usuarioRegistro_FK FOREIGN KEY (usuarioRegistro)
+  REFERENCES inventario.Usuario (usuario) ENABLE);
   
