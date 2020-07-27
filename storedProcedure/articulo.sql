1f8a69f5810374c676269b5ab5b33f38c6d991b4
Greivin <greivin95@gmail.com>
2020-07-26 22:25:04 -0600

    CRUD   ARTICULO

--- storedProcedure/articulo.sql
+++ storedProcedure/articulo.sql
@@ -30,12 +30,60 @@ VALUES (codigoSiguiente, 0);
 COMMIT;  
 END;
 
---DELETE
+--DELETE	 
+
+--BEGIN 
+--DROP PROCEDURE articuloDelete
+--END; 
+
+CREATE OR REPLACE PROCEDURE articuloDelete( 
+    @CODIGOARTICULO ARTICULO.CODIGOARTICULO%TYPE)
+AS 
+BEGIN 
+DELETE
+FROM   articulo
+WHERE  CODIGOARTICULO = @CODIGOARTICULO
+ 
+END
 
 
 --UPDATE
 
+--BEGIN 
+--DROP PROCEDURE codigoArticulo_Update
+--END 
+
+CREATE OR REPLACE PROCEDURE codigoArticulo_Update(
+    @codigoArticulo ARTICULO.CODIGOARTICULO%TYPE,
+    @descripcion ARTICULO.descripcion%TYPE(40 BYTE),
+    @cantMinima ARTICULO.cantMinima%TYPE,
+    @fechaCreacion ARTICULO.fechaCreacion%TYPE)
+AS 
+BEGIN 
+ 
+UPDATE articulo
+SET  codigoArticulo = @codigoArticulo,
+     descripcion = @descripcion, 
+     cantMinima = @cantMinima,
+     fechaCreacion = @fechaCreacion
+WHERE  codigoArticulo = @codigoArticulo
+END
+
 
 -- READ
+
+--BEGIN 
+--    DROP PROCEDURE articuloRead
+--END 
+
+CREATE OR REPLACE PROCEDURE articuloRead(
+    @codigoArticulo ARTICULO.CODIGOARTICULO%TYPE)
+AS 
+BEGIN 
+ 
+    SELECT codigoArticulo, descripcion, cantMinima, fechaCreacion
+    FROM   articulo  
+    WHERE  (codigoArticulo = @codigoArticulo) 
+END
 
 
