8ef94a380962794071388e5efd37cfdebcf09efe
yeco96 <yeisonpa.96@hotmail.com>
2020-07-26 20:11:46 -0600

    init project

---
+++ inventario/src/main/java/com/fidelitas/inventario/Inventario.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fidelitas.inventario;
+
+
+import com.fidelitas.inventario.Vista.inicio;
+/**
+ *
+ * @author yeiso
+ */
+public class Inventario {
+    
+    public static void main(String[] args) {
+       
+        inicio start = new inicio();
+        start.setVisible(true);
+    
+    }
+    
+}
