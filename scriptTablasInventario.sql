fe4229ef8e9ddd83bbeafda0e34113679fa5fa2f
Greivin <greivin95@gmail.com>
2020-07-26 19:22:27 -0600

    cambio de nombres tablas

--- scriptTablasInventario.sql
+++ scriptTablasInventario.sql
@@ -1,7 +1,15 @@
---drop table articulos;
+drop table articulo;
+drop table Proveedor;
+drop table Usuario;
+drop table articuloPrecio;
+drop table articuloExistencia;
+drop table Compra;
+drop table CompraDetalle;
+drop table Venta;
+drop table VentaDetalle;
+drop table Movimiento;
 
-
-create table inventario.articulos
+create table inventario.articulo
 (codigoArticulo int, descripcion VARCHAR2(40 BYTE), cantMinima NUMBER(18,3), fechaCreacion DATE,
 CONSTRAINT codigoArticulo_PK PRIMARY KEY (codigoArticulo));
 
@@ -17,50 +25,50 @@ create table inventario.articuloPrecio
 (codigoArticulo int, precio NUMBER(18,3), tarifaImpuesto NUMBER(5,3), porcentajeUtilidad NUMBER(5,3), montoCosto NUMBER(18,3),
 CONSTRAINT articuloPrecio_PK PRIMARY KEY (codigoArticulo),
 CONSTRAINT codigoArticulo_arti_FK FOREIGN KEY (codigoArticulo)
-  REFERENCES inventario.articulos (codigoArticulo) ENABLE);
+  REFERENCES inventario.articulo (codigoArticulo) ENABLE);
   
 create table inventario.articuloExistencia
 (codigoArticulo int, existencia NUMBER(18,3),
 CONSTRAINT articuloExistencia_PK PRIMARY KEY (codigoArticulo),
 CONSTRAINT articulo_articuloExistencia_FK FOREIGN KEY (codigoArticulo)
-  REFERENCES inventario.articulos (codigoArticulo) ENABLE);
+  REFERENCES inventario.articulo (codigoArticulo) ENABLE);
 
-create table inventario.Compras
+create table inventario.Compra
 (codigoCompra int, factura NUMBER(20,0), codigoProveedor int, montoCompra NUMBER(18,3), fechaCompra Date, usuarioRegistro varchar2(20 BYTE),
-CONSTRAINT compras_PK PRIMARY KEY (codigoCompra),
-CONSTRAINT compras_proveedor_FK FOREIGN KEY (codigoProveedor)
+CONSTRAINT compra_PK PRIMARY KEY (codigoCompra),
+CONSTRAINT compra_proveedor_FK FOREIGN KEY (codigoProveedor)
   REFERENCES inventario.Proveedor (codigoProveedor) ENABLE,
-CONSTRAINT compras_usuario_FK FOREIGN KEY (usuarioRegistro)
+CONSTRAINT compra_usuario_FK FOREIGN KEY (usuarioRegistro)
   REFERENCES inventario.Usuario (usuario) ENABLE);
 
   
-create table inventario.ComprasDetalle 
+create table inventario.CompraDetalle 
 (codigoCompra int,codigoArticulo int, cantidadCompra NUMBER(18,3), montoCosto NUMBER(18,3),
-CONSTRAINT ComprasDetalle_PK PRIMARY KEY (codigoCompra, codigoArticulo),
-CONSTRAINT ComprasDetalle_compra_FK FOREIGN KEY (codigoCompra)
-  REFERENCES inventario.Compras (codigoCompra) ENABLE,
-CONSTRAINT codigoArticulo_compras_FK FOREIGN KEY (codigoArticulo)
-  REFERENCES inventario.articulos (codigoArticulo) ENABLE);
+CONSTRAINT CompraDetalle_PK PRIMARY KEY (codigoCompra, codigoArticulo),
+CONSTRAINT CompraDetalle_compra_FK FOREIGN KEY (codigoCompra)
+  REFERENCES inventario.Compra (codigoCompra) ENABLE,
+CONSTRAINT codigoArticulo_compra_FK FOREIGN KEY (codigoArticulo)
+  REFERENCES inventario.articulo (codigoArticulo) ENABLE);
   
-create table inventario.Ventas  
+create table inventario.Venta 
 (codigoVenta int, factura NUMBER(20,0), identificacionCliente NUMBER(12,0), montoVenta NUMBER(18,3),fechaVenta Date, usuarioRegistro varchar2(20 BYTE),
-CONSTRAINT Ventas_PK PRIMARY KEY (codigoVenta),
-CONSTRAINT usuarioRegistro_Ventas_FK FOREIGN KEY (usuarioRegistro)
+CONSTRAINT Venta_PK PRIMARY KEY (codigoVenta),
+CONSTRAINT usuarioRegistro_Venta_FK FOREIGN KEY (usuarioRegistro)
   REFERENCES inventario.Usuario (usuario) ENABLE);
   
-create table inventario.VentasDetalle 
+create table inventario.VentaDetalle 
 (codigoVenta int, codigoArticulo int, precio  NUMBER(18,3),
-CONSTRAINT VentasDetalle_PK PRIMARY KEY (codigoVenta, codigoArticulo),
+CONSTRAINT VentaDetalle_PK PRIMARY KEY (codigoVenta, codigoArticulo),
 CONSTRAINT codigoVenta_detalle_FK FOREIGN KEY (codigoVenta)
-  REFERENCES inventario.Ventas (codigoVenta) ENABLE,
+  REFERENCES inventario.Venta (codigoVenta) ENABLE,
 CONSTRAINT codigoArticulo_venta_FK FOREIGN KEY (codigoArticulo)
-  REFERENCES inventario.articulos (codigoArticulo) ENABLE);
+  REFERENCES inventario.articulo (codigoArticulo) ENABLE);
   
-create table inventario.Movimientos
+create table inventario.Movimiento
 (codigoMovimiento int,codigoArticulo int, concepto varchar2 (20 BYTE), cantidadAntes NUMBER(18,3), cantidad NUMBER(18,3), cantidadDespues NUMBER(18,3), fechaRegistro Date, usuarioRegistro varchar2(20 BYTE),
-CONSTRAINT Movimientos_PK PRIMARY KEY (codigoMovimiento),
-CONSTRAINT codigoArticulo_Movimientos_FK FOREIGN KEY (codigoArticulo)
-  REFERENCES inventario.articulos (codigoArticulo) ENABLE,
- CONSTRAINT usuarioRegistro_Movimientos_FK FOREIGN KEY (usuarioRegistro)
+CONSTRAINT Movimiento_PK PRIMARY KEY (codigoMovimiento),
+CONSTRAINT codigoArticulo_Movimiento_FK FOREIGN KEY (codigoArticulo)
+  REFERENCES inventario.articulo (codigoArticulo) ENABLE,
+ CONSTRAINT usuarioRegistro_Movimiento_FK FOREIGN KEY (usuarioRegistro)
   REFERENCES inventario.Usuario (usuario) ENABLE);
   
