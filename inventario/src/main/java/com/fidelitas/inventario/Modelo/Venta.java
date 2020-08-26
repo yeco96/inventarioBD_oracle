11da44a1e40944de332a2ab6cb1b4a984ff06889
Greivin <greivin95@gmail.com>
2020-08-25 23:47:02 -0600

    vetnta y venta detalle

---
+++ inventario/src/main/java/com/fidelitas/inventario/Modelo/Venta.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fidelitas.inventario.Modelo;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author Josue
+ */
+public class Venta {
+    private int codigoVenta;
+    private int factura;
+    private int identificacionCliente;
+    private BigDecimal montoVenta;
+    private Date fechaVenta;
+    private String usuarioRegistro;
+    
+     private List<VentaDetalle> detalle;
+    
+    public int getCodigoVenta() {
+        return codigoVenta;
+    }
+
+    public void setCodigoVenta(int codigoVenta) {
+        this.codigoVenta = codigoVenta;
+    }
+
+    public int getFactura() {
+        return factura;
+    }
+
+    public void setFactura(int factura) {
+        this.factura = factura;
+    }
+
+    public int getIdentificacionCliente() {
+        return identificacionCliente;
+    }
+
+    public void setIdentificacionCliente(int identificacionCliente) {
+        this.identificacionCliente = identificacionCliente;
+    }
+
+    public BigDecimal getMontoVenta() {
+        return montoVenta;
+    }
+
+    public void setMontoVenta(BigDecimal montoVenta) {
+        this.montoVenta = montoVenta;
+    }
+
+    public Date getFechaVenta() {
+        return fechaVenta;
+    }
+
+    public void setFechaVenta(Date fechaVenta) {
+        this.fechaVenta = fechaVenta;
+    }
+
+    public String getUsuarioRegistro() {
+        return usuarioRegistro;
+    }
+
+    public void setUsuarioRegistro(String usuarioRegistro) {
+        this.usuarioRegistro = usuarioRegistro;
+    }
+
+    public List<VentaDetalle> getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(List<VentaDetalle> detalle) {
+        this.detalle = detalle;
+    }
+    
+   
+    
+    
+}
---
+++ inventario/src/main/java/com/fidelitas/inventario/Modelo/VentaDetalle.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fidelitas.inventario.Modelo;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author Josue
+ */
+public class VentaDetalle {
+    
+    private int codigoVenta;
+    private int codigoArticulo;
+    private BigDecimal precio;
+
+    public int getCodigoVenta() {
+        return codigoVenta;
+    }
+
+    public void setCodigoVenta(int codigoVenta) {
+        this.codigoVenta = codigoVenta;
+    }
+
+    public int getCodigoArticulo() {
+        return codigoArticulo;
+    }
+
+    public void setCodigoArticulo(int codigoArticulo) {
+        this.codigoArticulo = codigoArticulo;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+    
+    
+   
+    
+}
